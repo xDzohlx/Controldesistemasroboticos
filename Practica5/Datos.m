5bcd96d09ba77bbc48e8fc3d5721ccdf44a7c5aa
Carlos Vega <69025860+xDzohlx@users.noreply.github.com>
2020-09-07 00:51:36 -0500

    Create Datos.m

---
+++ Practica5/Datos.m
@@ -0,0 +1,13 @@
+%%codigo que utilize en la linea de comandos para sacar los datos del simout
+Robot=out.data(out.tout)
+%regresa
+%  timeseries
+%
+%  Common Properties:
+%            Name: ''
+%            Time: [74x1 double]
+%        TimeInfo: [1x1 tsdata.timemetadata]
+%            Data: [74x2 double]
+%        DataInfo: [1x1 tsdata.datametadata]
+%
+%  More properties, Methods

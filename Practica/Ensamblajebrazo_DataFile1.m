5e116ef7d004fa7fff384d44ed3665af0353536e
Carlos Vega <69025860+xDzohlx@users.noreply.github.com>
2020-09-07 21:19:34 -0500

    Add files via upload
    Archivos de solid y matlab con simulink

---
+++ Practica/Ensamblajebrazo_DataFile1.m
@@ -0,0 +1,87 @@
+% Simscape(TM) Multibody(TM) version: 7.0
+
+% This is a model data file derived from a Simscape Multibody Import XML file using the smimport function.
+% The data in this file sets the block parameter values in an imported Simscape Multibody model.
+% For more information on this file, see the smimport function help page in the Simscape Multibody documentation.
+% You can modify numerical values, but avoid any other changes to this file.
+% Do not add code to this file. Do not edit the physical units shown in comments.
+
+%%%VariableName:smiData
+
+
+%============= RigidTransform =============%
+
+%Initialize the RigidTransform structure array by filling in null values.
+smiData.RigidTransform(3).translation = [0.0 0.0 0.0];
+smiData.RigidTransform(3).angle = 0.0;
+smiData.RigidTransform(3).axis = [0.0 0.0 0.0];
+smiData.RigidTransform(3).ID = '';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(1).translation = [0 140 1.5000000000000013];  % mm
+smiData.RigidTransform(1).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(1).axis = [1 0 0];
+smiData.RigidTransform(1).ID = 'B[Brazo-1:-:Hombro-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(2).translation = [7.1054273576010019e-15 7.1054273576010019e-15 0];  % mm
+smiData.RigidTransform(2).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(2).axis = [1 0 0];
+smiData.RigidTransform(2).ID = 'F[Brazo-1:-:Hombro-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(3).translation = [-53.105510001944737 60.741024545971747 303.00000000002137];  % mm
+smiData.RigidTransform(3).angle = 0;  % rad
+smiData.RigidTransform(3).axis = [0 0 0];
+smiData.RigidTransform(3).ID = 'RootGround[Hombro-1]';
+
+
+%============= Solid =============%
+%Center of Mass (CoM) %Moments of Inertia (MoI) %Product of Inertia (PoI)
+
+%Initialize the Solid structure array by filling in null values.
+smiData.Solid(2).mass = 0.0;
+smiData.Solid(2).CoM = [0.0 0.0 0.0];
+smiData.Solid(2).MoI = [0.0 0.0 0.0];
+smiData.Solid(2).PoI = [0.0 0.0 0.0];
+smiData.Solid(2).color = [0.0 0.0 0.0];
+smiData.Solid(2).opacity = 0.0;
+smiData.Solid(2).ID = '';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(1).mass = 0.070149909348255948;  % kg
+smiData.Solid(1).CoM = [0 -1.3213515470704098 0];  % mm
+smiData.Solid(1).MoI = [518.03901796027685 4.6748583654377907 522.60865146169238];  % kg*mm^2
+smiData.Solid(1).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(1).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(1).opacity = 1;
+smiData.Solid(1).ID = 'Brazo*:*Predeterminado';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(2).mass = 0.012825909348255947;  % kg
+smiData.Solid(2).CoM = [0 0 1.5];  % mm
+smiData.Solid(2).MoI = [1.8038813654377899 1.8038813654377905 3.5885238668531962];  % kg*mm^2
+smiData.Solid(2).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(2).color = [0.89803921568627454 0.91764705882352937 0.92941176470588238];
+smiData.Solid(2).opacity = 1;
+smiData.Solid(2).ID = 'Hombro*:*Predeterminado';
+
+
+%============= Joint =============%
+%X Revolute Primitive (Rx) %Y Revolute Primitive (Ry) %Z Revolute Primitive (Rz)
+%X Prismatic Primitive (Px) %Y Prismatic Primitive (Py) %Z Prismatic Primitive (Pz) %Spherical Primitive (S)
+%Constant Velocity Primitive (CV) %Lead Screw Primitive (LS)
+%Position Target (Pos)
+
+%Initialize the RevoluteJoint structure array by filling in null values.
+smiData.RevoluteJoint(1).Rz.Pos = 0.0;
+smiData.RevoluteJoint(1).ID = '';
+
+smiData.RevoluteJoint(1).Rz.Pos = -1.5138972085569942;  % deg
+smiData.RevoluteJoint(1).ID = '[Brazo-1:-:Hombro-1]';
+

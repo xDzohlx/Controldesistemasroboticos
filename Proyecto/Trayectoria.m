9ef129d8de018513966f18b85d4e9e990c113043
Carlos Vega <69025860+xDzohlx@users.noreply.github.com>
2020-09-15 07:46:17 -0500

    Update Trayectoria.m

--- Proyecto/Trayectoria.m
+++ Proyecto/Trayectoria.m
@@ -1,6 +1,6 @@
 function [xd,yd] = fcn(t)
 %%circulo
-hx=0.2;
+hx=0.25;
 hy=0;
 a=1;
 b=2;
@@ -10,4 +10,3 @@ f=1/T;
 w=2*pi*f;
 xd=hx+0.05*((a-b)*cos(w*t)+b*cos(w*t*((a/b)-1)));
 yd=hy+0.05*((a-b)*sin(w*t)+b*sin(w*t*((a/b)-1)));
-
